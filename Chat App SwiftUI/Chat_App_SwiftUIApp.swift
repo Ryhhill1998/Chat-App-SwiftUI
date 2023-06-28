ff1a24c02d95e4b1940446957b43d6326f4a967c
ryhhill1998 <ryhhill1998@outlook.com>
2023-06-28 09:10:03 +0100

    Initial Commit

---
+++ Chat App SwiftUI/Chat_App_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chat_App_SwiftUIApp.swift
+//  Chat App SwiftUI
+//
+//  Created by Ryan Henzell-Hill on 28/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct Chat_App_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
